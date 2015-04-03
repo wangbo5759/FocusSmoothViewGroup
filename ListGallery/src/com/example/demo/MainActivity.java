a27aef94c44aa5707eae9c89830f3adab4503402
wangbo5759 <challengerwang@163.com>
2015-04-03 17:03:54 +0800

    commit listgallery view and demo

--- ListGallery/src/com/example/demo/MainActivity.java
+++ ListGallery/src/com/example/demo/MainActivity.java
@@ -1,19 +1,76 @@
 package com.example.demo;
 
+import com.example.listgallery.ListGallery;
 import com.example.listgallery.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.listgallery.AdapterView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener{
+	private ListGallery mListGallery;
+	
+	private static final int GALLERY_ITEM_NUMBER = Integer.MAX_VALUE;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mListGallery = (ListGallery)findViewById(R.id.listgallery);
+		mListGallery.setOnItemClickListener(this);
+		mListGallery.setAdapter(new BaseAdapter() {
+			
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				Context context = parent.getContext();
+				if (convertView == null){
+					convertView = new ImageView(context);
+					((ImageView)convertView).setImageResource(R.drawable.launcher_default_poster_hubo);
+					convertView.setFocusable(true);
+					convertView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+						
+						@Override
+						public void onFocusChange(View v, boolean hasFocus) {
+							if (hasFocus){
+								v.setAlpha(0.6f);
+							} else {
+								v.setAlpha(1.0f);
+							}
+						}
+					});
+				} else {
+					((ImageView)convertView).setImageResource(R.drawable.launcher_default_poster_hubo);
+				}
+				return convertView;
+			}
+			
+			@Override
+			public long getItemId(int position) {
+				// TODO Auto-generated method stub
+				return position;
+			}
+			
+			@Override
+			public Object getItem(int position) {
+				// TODO Auto-generated method stub
+				return position;
+			}
+			
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return GALLERY_ITEM_NUMBER;
+			}
+		});
 	}
 
 	@Override
@@ -35,4 +92,9 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Toast.makeText(this, "Click position " + position, 1000).show();
+	}
 }
